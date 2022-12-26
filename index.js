8c260414def020619687f28a27afa1c0b2f5f1c1
BobBrian <rellrocker235@gmail.com>
2022-12-26 16:52:23 +0200

    Testing this out

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const PORT  = process.env.PORT || 5000;
 //We make this new Addition to Cors
 
 var corsOptions = {
-  origin: "https://perntodo-vq2v.onrender.com",
+  origin: "https://perntodo-vq2v.onrender.com", 
+  methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"]
 }
 
 app.use(express.json());
@@ -24,6 +25,7 @@ app.post("/todos", cors(corsOptions), async (req, res) => {
     try {
       const { description } = req.body;
       const newTodo = await pool.query( "INSERT INTO todo (description) VALUES($1) RETURNING *", [description]);
+      res.header("Access-Control-Allow-Origin", "https://perntodo-vq2v.onrender.com")
       res.json(newTodo.rows[0]);
     } catch (err) {
       console.error(err.message);
@@ -35,6 +37,7 @@ app.post("/todos", cors(corsOptions), async (req, res) => {
 app.get("/todos",cors(corsOptions), async (req, res) => {
     try {
       const allTodos = await pool.query("SELECT * FROM todo");
+      res.header("Access-Control-Allow-Origin", "https://perntodo-vq2v.onrender.com")
       res.json(allTodos.rows);
     } catch (err) {
       console.error(err.message);
@@ -47,6 +50,7 @@ app.get("/todos",cors(corsOptions), async (req, res) => {
     try {
       const { id } = req.params;
       const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+      res.header("Access-Control-Allow-Origin", "https://perntodo-vq2v.onrender.com")
       res.json(todo.rows[0]);
     } catch (err) {
       console.error(err.message);
@@ -60,6 +64,7 @@ app.put("/todos/:id", cors(corsOptions), async (req, res) => {
       const { id } = req.params;
       const { description } = req.body;
       const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2",[description, id]);
+      res.header("Access-Control-Allow-Origin", "https://perntodo-vq2v.onrender.com")
       res.json("Todo was updated!");
     } catch (err) {
       console.error(err.message);
@@ -72,6 +77,7 @@ app.delete("/todos/:id", cors(corsOptions), async (req, res) => {
     try {
       const { id } = req.params;
       const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
+      res.header("Access-Control-Allow-Origin", "https://perntodo-vq2v.onrender.com")
       res.json("Todo was deleted!");
     } catch (err) {
       console.log(err.message);
